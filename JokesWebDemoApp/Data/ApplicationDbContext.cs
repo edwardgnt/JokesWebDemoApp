cb65f73bad56e7630161c7b562a85a4db57d05e4
edward.guereque <edward.guereque@WEBDEV1>
2023-01-23 21:06:43 -0800

    Add project files.

---
+++ JokesWebDemoApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokesWebDemoApp.Models;
+
+namespace JokesWebDemoApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesWebDemoApp.Models.Joke> Joke { get; set; } = default!;
+    }
+}
